77ef04a044185d1ca9a39c2696271356eaaf065c
Higorbraga01 <higorbraga00@gmail.com>
2021-10-22 17:50:23 -0300

    Simplificando entidades com Embeddable

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,10 +1,6 @@
 package br.com.alura.loja.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "clientes")
@@ -13,12 +9,12 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+
+	@Embedded
+	private DadosPessoais dadosPessoais;
 
 	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome,cpf);
 	}
 
 	public Cliente() {
@@ -33,19 +29,14 @@ public class Cliente {
 	}
 
 	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
+		return dadosPessoais.getNome();
 	}
 
 	public String getCpf() {
-		return cpf;
+		return dadosPessoais.getCpf();
 	}
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
-
 }
